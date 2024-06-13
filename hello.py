8d47beb746f76b2d214767ccb48a8747b71bd1b2
arpitparekh <arpitparekh9@gmail.com>
2024-06-13 08:26:17 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,44 @@
+print("hello")
+print("hello")
+print("hello")
+
+# builtin function calling
+
+print("hello")   
+print(12.34)
+
+# compiler(c/c++) // c c++ java
+# interpreter(c/c++)  // python
+# high level -> manchine langugae
+# python default libraries  // download python web
+
+# 12 13
+
+a=1
+a=13   # reassigning a value to the variable
+a = "bascom" # group of character
+a = 'b'
+a = ' '
+a = "bascom computers"
+a = 'bascom'
+#  "hello"
+
+a,b,c = "Hello",12.34,13
+
+print(a)
+print(b)
+print(c)
+
+x = y = z = 12
+
+print(x,y,z)
+
+y = z = 77  
+
+print(x,y,z)
+
+z = "bascom"
+
+print(x,y,z)
+
+print(x+y)
