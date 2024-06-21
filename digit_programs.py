b4d1ebb670832d1627e26b887d86abf571e89864
arpitparekh <arpitparekh9@gmail.com>
2024-06-21 08:20:11 +0530

    fibonacci series

--- digit_programs.py
+++ digit_programs.py
@@ -12,6 +12,8 @@
 # 64     / 10 = 6
 # 6      / 10 = 0
 
+import random   # generate randeom number
+
 myNum  = int(input("Please Enter Number : "))
 duplicate = myNum
 
@@ -34,6 +36,7 @@ duplicate = myNum2
 rev = 0
 
 while myNum2!=0:                    # 1234  # 123   12  1 
+    
     last_digit = myNum2 % 10        # 4     #  3    2   1
     
     rev = rev*10 + last_digit       # 4   #  43   # 432  # 4321
@@ -46,11 +49,50 @@ print(f"revese of number is {rev}")
 # palindrome or not
 
 print(f"{duplicate} is {'palimdrome' if duplicate==rev else 'not plindrome'}")
+
+
+# find factorial of number
+
+# 7! = 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+i = 7
+duplicate = i
+factorial = 1
+
+while i>=1:    # 7
     
+    factorial *= i
     
+    i -= 1
+    
+print(f"Factorial of {duplicate} is {factorial}")
 
+# 7! = 5040
 
+# generate randeom number
 
+print(random.randint(1,100))
 
+# fibonacci series
 
+# 1 1 2 3 5 8 13
 
+v = int(input("Enter Number : "))
+
+# a = 1    # 1  # 1  # 2   # 3  # 5   #  8
+# b = 1    # 1  # 2  # 3   # 5  # 8   # 13
+# c = a+b  # 2  # 3  # 5   # 8  # 13  #  21
+
+# a = b  b = c
+
+a = 1
+b = 1
+
+while v>=1:
+    
+    print(a,end=' ')
+    c = a+b
+    a = b
+    b = c
+    
+    v-=1
