6745abf3455d818443c5c655a4304063921887b7
arpitparekh <arpitparekh9@gmail.com>
2024-07-06 08:32:00 +0530

    swap_first_last_digit

--- programs/first_digit_last_digit.py
+++ programs/first_digit_last_digit.py
@@ -1,18 +1,29 @@
-num = int(input("Please Enter Number : "))  # 2345
+num = int(input("Please Enter Number : "))  # 2345 - 2000
+original = num
 
 last_digit = num%10  # 5
 
 new_last_digit = 0
 
+count = 0
+
 while num!=0:
+    count+=1
     new_last_digit = num%10  # 5  # 4 # 3  # 2
     num = num//10   # 234 # 23  # 2 # 0
     
-
 print(f"First Digit is : {new_last_digit}")
 print(f"Last Digit is : {last_digit}") 
 
+original = original-last_digit   # 2340
 
+original = original - new_last_digit*(10**(count-1)) # 340
+
+original = original + new_last_digit # 342
+
+original = original +  last_digit*(10**(count-1))
+
+print(f"Swapped digit number is {original}")
 
 # 4356
 # 4 , 6
