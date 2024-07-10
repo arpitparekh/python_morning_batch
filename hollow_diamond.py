49f77652da35d878a7175cb81174c27be7e418f7
arpitparekh <arpitparekh9@gmail.com>
2024-07-10 11:56:45 +0530

    hollow_diagram

--- hollow_diamond.py
+++ hollow_diamond.py
@@ -17,16 +17,12 @@ for x in range(1,6):
     
     for y in range(x,6):
         print("*",end="")
-    
     for z in range(3,2*x+1):    # (3,3)(3,5)()
         print(" ",end="")
-    
     for y in range(1,12-2*x): # (1,11)
-        print("*",end="") 
-     
+        print("*",end="")
     for z in range(3,2*x+1):    # (3,3)(3,5)()
         print(" ",end="")
-        
     for y in range(1,12-2*x): # (1,11)
         print("*",end="") 
     for z in range(3,2*x+1):    # (3,3)(3,5)()
@@ -68,6 +64,7 @@ print()
 # **********
 
 # class and objects
+# functional programming
 
 
 
